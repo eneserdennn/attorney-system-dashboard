6b7e953c6737846065489727d7c474274227a844
Enes Erden <eneserdendev@gmail.com>
2023-03-21 16:01:42 +0300

    Single Client Page style fixed

--- src/pages/client/SingleClient.js
+++ src/pages/client/SingleClient.js
@@ -4,87 +4,113 @@ import React, { useEffect } from "react";
 import { Col, Row, Space, Grid } from "antd";
 import DeleteIcon from "@mui/icons-material/Delete";
 import dayjs from "dayjs/";
-const SingleClient = () => {
-  const { client } = useGetClient(window.location.pathname);
 
-  return (
-    <MainCard>
-      <MainCard>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Space>
-            <h2>
-              {client.name} {client.surname}
-            </h2>
-          </Space>
-          <Space>
-            <DeleteIcon />
-          </Space>
-        </div>
-      </MainCard>
-      <div style={{ marginTop: "20px" }}>
-        <Row gutter={16}>
-          <Col span={6}>
-            <MainCard title="Contact info">
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "flex-start",
-                  flexDirection: "column",
-                }}
-              >
-                <Space>email: {client.email}</Space>
-                <Space>phone: {client.phone}</Space>
-                <Space>city: {client.city}</Space>
-                <Space>address: {client.address}</Space>
-              </div>
+const SingleClient = () => {
+    const { client } = useGetClient(window.location.pathname);
+
+    return (
+        <MainCard>
+            <MainCard>
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <Space>
+                        <h2>
+                            {client.name} {client.surname}
+                        </h2>
+                    </Space>
+                    <Space>
+                        <DeleteIcon />
+                    </Space>
+                </div>
             </MainCard>
-          </Col>
-          <Col span={6}>
-            <MainCard title="Events/Calender/Folders">
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "flex-start",
-                  flexDirection: "column",
-                }}
-              >
-                <Space>Events: {client.events}</Space>
-                <Space>Folders: {client.folders}</Space>
-                {client.isOrganization === false ? null : (
-                  <Space>Organization: {client.organization}</Space>
-                )}
-                <Space>
-                  Created At: {dayjs(client.createdAt).format("DD-MM-YYYY")}
-                </Space>
-                <Space>Quality: {client.quality}</Space>
-              </div>
-            </MainCard>
-          </Col>
-          <Col span={6}>
-            <MainCard title="Payment Info">
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "flex-start",
-                  flexDirection: "column",
-                }}
-              >
-                <Space>Currency: {client.currency}</Space>
-                <Space>Vat Rate: </Space>
-                <Space>Hour Rate:</Space>
-              </div>
-            </MainCard>
-          </Col>
-        </Row>
-      </div>
-    </MainCard>
-  );
+            <div style={{ marginTop: "20px" }}>
+                <Row gutter={16}>
+                    <Col span={18}>
+                        <MainCard title="Contact info">
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "flex-start",
+                                    flexDirection: "column",
+                                }}
+                            >
+                                <Space>email: {client.email}</Space>
+                                <Space>phone: {client.phone}</Space>
+                                <Space>city: {client.city}</Space>
+                                <Space>address: {client.address}</Space>
+                            </div>
+                        </MainCard>
+                    </Col>
+                    <Col span={6}>
+                        <MainCard title="Payment Info">
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "flex-start",
+                                    flexDirection: "column",
+                                }}
+                            >
+                                <Space>Currency: {client.currency}</Space>
+                                <Space>Vat Rate: </Space>
+                                <Space>Hour Rate:</Space>
+                            </div>
+                        </MainCard>
+                    </Col>
+                </Row>
+                <Row gutter={16} style={{ marginTop: "20px" }}>
+                    <Col span={6}>
+                        <MainCard title="Events">
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "flex-start",
+                                    flexDirection: "column",
+                                }}
+                            >
+                                <Space>{client.events}</Space>
+                            </div>
+                        </MainCard>
+                    </Col>
+                    <Col span={6}>
+                        <MainCard title="Calender">
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "flex-start",
+                                    flexDirection: "column",
+                                }}
+                            >
+                                <Space>Calender: </Space>
+                            </div>
+                        </MainCard>
+                    </Col>
+                    <Col span={6}>
+                        <MainCard title="Folders">
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "flex-start",
+                                    flexDirection: "column",
+                                }}
+                            >
+                                <Space>Folders: {client.folders}</Space>
+                                {client.isOrganization === false ? null : (
+                                    <Space>Organization: {client.organization}</Space>
+                                )}
+                                <Space>Created At: {dayjs(client.createdAt).format("DD-MM-YYYY")}</Space>
+                                <Space>Quality: {client.quality}</Space>
+                            </div>
+                        </MainCard>
+                    </Col>
+                </Row>
+            </div>
+        </MainCard>
+    );
 };
 
 export default SingleClient;
